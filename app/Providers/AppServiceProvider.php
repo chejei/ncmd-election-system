328290695f7b08e3f6fe855bab474e4e039d1f34
Vallecera, Chejei <chejei.vallecera@gmail.com>
2025-10-04 13:36:05 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Storage;
 use App\Services\SettingService;
 
 class AppServiceProvider extends ServiceProvider
