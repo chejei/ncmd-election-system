2737c2616a401ccc82551e99bcded082e08dfbce
Vallecera, Chejei <chejei.vallecera@gmail.com>
2025-10-04 16:37:49 +0800

    Update ElectionResult.jsx

--- resources/js/pages/ElectionResult.jsx
+++ resources/js/pages/ElectionResult.jsx
@@ -158,9 +158,9 @@ export default function ElectionResult() {
                 <div className="mx-auto sm:px-7 px-4 max-w-screen-xl overlay-text">
                     <div className="overflow-x-auto">
                         {stats.election_count
-                            .filter((pos) => pos.candidates.length > 0)
+                            .filter((pos) => pos.approved_candidates.length > 0)
                             .map((pos) => {
-                                const sorted = [...pos.candidates].sort(
+                                const sorted = [...pos.approved_candidates].sort(
                                     (a, b) => b.vote_count - a.vote_count
                                 );
 
